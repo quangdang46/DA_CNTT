4fb17875405776a3293e303fb2936d7a1cd3a9a4
quangdang46 <tranquangdang21@gmail.com>
2025-01-11 23:13:32 +0700

    backend

---
+++ backend/app/Repositories/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function getAllPaginate()
+    {
+        return User::paginate(15);
+    }
+}
