74ed2572c670fb13ee86dbdb0a593386e4f50f04
quangdang46 <tranquangdang21@gmail.com>
2025-01-22 17:18:18 +0700

    fix

--- frontend/src/shared/components/ui/Account/DashboardTab.tsx
+++ frontend/src/shared/components/ui/Account/DashboardTab.tsx
@@ -6,42 +6,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function DashboardTab() {
-  const { activeTab } = useTabs();
-  // const [account, setAccount] = useState<UserResType>({} as UserResType); // UserResType hoặc null
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [isError, setIsError] = useState(false);
-
-  // useEffect(() => {
-  //   const fetchUser = async () => {
-  //     try {
-  //       setIsLoading(true);
-  //       const response = await accountApiRequest.me();
-  //       if (response.success) {
-  //         setAccount(response.data.user); // Cập nhật thông tin người dùng
-  //       } else {
-  //         console.error("Failed to fetch user data:", response.message);
-  //         setIsError(true);
-  //       }
-  //     } catch (error) {
-  //       console.error("Error while fetching user:", error);
-  //       setIsError(true); // Cập nhật trạng thái lỗi
-  //     } finally {
-  //       setIsLoading(false); // Kết thúc loading
-  //     }
-  //   };
-
-  //   fetchUser();
-  // }, []);
+  const { activeTab, setActiveTab } = useTabs();
 
   const { user, token } = useSelector((state: RootState) => state.auth);
 
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // if (isError) {
-  //   return <div>Error occurred while fetching user data.</div>;
-  // }
   return (
     <div
       className="woocommerce-MyAccount-content"
@@ -60,17 +28,32 @@ export default function DashboardTab() {
 
       <p>
         From your account dashboard you can view your
-        <a href="https://techmarket.madrasthemes.com/my-account/orders/">
+        <strong
+          className="text-danger"
+          style={{ cursor: "pointer" }}
+          onClick={() => setActiveTab("account-orders")}
+        >
+          {" "}
           recent orders
-        </a>
+        </strong>
         , manage your
-        <a href="https://techmarket.madrasthemes.com/my-account/edit-address/">
+        <strong
+          className="text-danger"
+          style={{ cursor: "pointer" }}
+          onClick={() => setActiveTab("account-address")}
+        >
+          {" "}
           shipping and billing addresses
-        </a>
+        </strong>
         , and
-        <a href="https://techmarket.madrasthemes.com/my-account/edit-account/">
+        <strong
+          className="text-danger"
+          style={{ cursor: "pointer" }}
+          onClick={() => setActiveTab("account-details")}
+        >
+          {" "}
           edit your password and account details
-        </a>
+        </strong>
         .
       </p>
     </div>
