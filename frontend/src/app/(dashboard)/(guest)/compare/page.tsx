241f799fa368571700bda99ae8f7d01f8ddebed9
quangdang46 <tranquangdang21@gmail.com>
2025-01-27 22:31:16 +0700

    compare

---
+++ frontend/src/app/(dashboard)/(guest)/compare/page.tsx
@@ -0,0 +1,30 @@
+import WrapperContent from "@/shared/components/layouts/WrapperContent";
+import React from "react";
+
+export default function page() {
+  return (
+    <WrapperContent>
+      <div
+        id="post-467"
+        className="post-467 page type-page status-publish hentry"
+      >
+        <div className="entry-content">
+          <div className="compare-empty">
+            <h1 className="lead-title text-center cart-empty">
+              No products were added to the compare table{" "}
+            </h1>
+            <p className="return-to-shop">
+              <a
+                className="button"
+                href="https://techmarket.madrasthemes.com/shop/"
+              >
+                <i className="icon fa fa-mail-reply" />
+                Return To Shop{" "}
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </WrapperContent>
+  );
+}
