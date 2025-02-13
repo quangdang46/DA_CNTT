5432aac8db6ee18a5182b2fe66ec44accdcd2f69
quangdang46 <tranquangdang21@gmail.com>
2025-02-13 22:26:40 +0700

    fix

--- backend/app/Http/Controllers/AdminController.php
+++ backend/app/Http/Controllers/AdminController.php
@@ -1,3 +1,181 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+    //
+    public function index()
+    {
+        try {
+            try {
+                $user = JWTAuth::parseToken()->authenticate(); // Lấy thông tin người dùng từ token
+                $role = $user->role;
+                if ($role != 'admin') {
+                    return response()->json([
+                        "success" => false,
+                        'status' => 'error',
+                        'message' => 'You are not admin',
+                        'data' => null
+                    ]);
+                }
+                // total user
+                $totalUser = \App\Models\User::count();
+                // total product
+                $totalProduct = \App\Models\Product::count();
+                // total order
+                $totalOrder = \App\Models\Order::count();
+                // total get all payment
+                $payment = \App\Models\Payment::orderBy('id', 'desc')->take(5)->get();
+
+
+                $revenueByHour = \App\Models\Order::where('status', 'delivered')
+                    ->select(DB::raw("DATE_FORMAT(order_time, '%Y-%m-%d %H:00:00') as time"), DB::raw('SUM(total_price) as revenue'))
+                    ->groupBy('time')
+                    ->orderBy('time', 'ASC')
+                    ->get();
+                return response()->json([
+                    "success" => true,
+                    'status' => 'success',
+                    'message' => 'Get data success',
+                    'data' => [
+                        'totalUser' => $totalUser,
+                        'totalProduct' => $totalProduct,
+                        'totalOrder' => $totalOrder,
+                        'payment' => $payment,
+                        'revenueByHour' => $revenueByHour
+                    ]
+                ]);
+            } catch (\Throwable $th) {
+                return response()->json([
+                    "success" => false,
+                    'status' => 'error',
+                    'message' => 'User not authenticated',
+                    'data' => null
+                ]);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'status' => 500,
+                'message' => 'Something went wrong',
+                'data' => [],
+                'error' => $th->getMessage()
+            ]);
+        }
+    }
+
+    public function getTransaction(Request $request)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate(); // Lấy thông tin người dùng từ token
+            $role = $user->role;
+            if ($role != 'admin') {
+                return response()->json([
+                    "success" => false,
+                    'status' => 'error',
+                    'message' => 'You are not admin',
+                    'data' => null
+                ]);
+            }
+            $perPage = $request->query('per_page', null);
+            $page = $request->query('page', null);
+            $payments = \App\Models\Payment::query()
+                ->paginate($perPage, ['*'], 'page', $page);
+
+
+            return $payments;
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'data' => null
+            ]);
+        }
+    }
+
+    public function updateTransaction(Request $request, $id)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate(); // Lấy thông tin người dùng từ token
+            $role = $user->role;
+            if ($role != 'admin') {
+                return response()->json([
+                    "success" => false,
+                    'status' => 'error',
+                    'message' => 'You are not admin',
+                    'data' => null
+                ]);
+            }
+            $payment = \App\Models\Payment::find($id);
+            if (!$payment) {
+                return response()->json([
+                    "success" => false,
+                    'status' => 'error',
+                    'message' => 'Payment not found',
+                    'data' => null
+                ]);
+            }
+            $payment->update($request->all());
+            return response()->json([
+                "success" => true,
+                'status' => 'success',
+                'message' => 'Update payment success',
+                'data' => $payment
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'data' => null
+            ]);
+        }
+    }
+
+    public function deleteTransaction($id)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate(); // Lấy thông tin người dùng của token
+            $role = $user->role;
+            if ($role != 'admin') {
+                return response()->json([
+                    "success" => false,
+                    'status' => 'error',
+                    'message' => 'You are not admin',
+                    'data' => null
+                ]);
+            }
+            $payment = \App\Models\Payment::find($id);
+            if (!$payment) {
+                return response()->json([
+                    "success" => false,
+                    'status' => 'error',
+                    'message' => 'Payment not found',
+                    'data' => null
+                ]);
+            }
+            $payment->delete();
+            return response()->json([
+                "success" => true,
+                'status' => 'success',
+                'message' => 'Delete payment success',
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                'status' => 'error',
+                'message' => $th->getMessage(),
+                'data' => null
+            ]);
+        }
+    }
+    // //////// Order //////////
 
     public function getOrders(Request $request)
     {
@@ -25,7 +203,6 @@
                 'data' => null
             ]);
         }
-
     }
 
     public function updateOrder(Request $request, $id)
@@ -105,3 +282,7 @@
             ]);
         }
     }
+
+    // //////// Order //////////
+
+}
--- frontend/src/shared/apiRequests/order.ts
+++ frontend/src/shared/apiRequests/order.ts
@@ -1,4 +1,86 @@
-  useGetOrderPaginate: ({ page, perPage }: { page: number; perPage: number }) => {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import apiClient from "@/shared/config/apiClient";
+import {
+  OrderAdminResType,
+  OrderBodyAdmin,
+  OrderGetResType,
+  OrderType,
+} from "@/shared/types/OrderTypes";
+import { ResType } from "@/shared/types/resType";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+const orderApiRequest = {
+  useCheckout: () => {
+    return useMutation({
+      mutationFn: async (payload: OrderType) => {
+        try {
+          const response = await apiClient.post<OrderType, any>(
+            "/orders",
+            payload
+          );
+          return response;
+        } catch (error) {
+          console.error("API error:", error);
+          throw error;
+        }
+      },
+    });
+  },
+  useReturnPayment: () => {
+    return useMutation({
+      mutationFn: async (payload: any) => {
+        try {
+          const response = await apiClient.post<any, any>(
+            "/vnpay/paymentReturn",
+            payload
+          );
+          return response;
+        } catch (error) {
+          console.error("API error:", error);
+          throw error;
+        }
+      },
+    });
+  },
+  useTrackOrder: () => {
+    return useMutation({
+      mutationFn: async (payload: any) => {
+        try {
+          const response = await apiClient.post<any, any>(
+            "/orders/track-order",
+            payload
+          );
+          return response;
+        } catch (error) {
+          console.error("API error:", error);
+          throw error;
+        }
+      },
+    });
+  },
+  useGetOrder: () => {
+    return useQuery({
+      queryKey: ["orders"],
+      queryFn: async () => {
+        try {
+          const response = await apiClient.get<OrderGetResType>("/orders");
+          return response;
+        } catch (error) {
+          console.error("API error:", error);
+          throw error;
+        }
+      },
+    });
+  },
+
+  //////// order//////
+  useGetOrderPaginate: ({
+    page,
+    perPage,
+  }: {
+    page: number;
+    perPage: number;
+  }) => {
     return useQuery<OrderAdminResType, Error>({
       queryKey: ["orders-paginate", page, perPage],
       queryFn: async () => {
@@ -54,3 +136,7 @@
       },
     });
   },
+  //////// order//////
+};
+
+export default orderApiRequest;
